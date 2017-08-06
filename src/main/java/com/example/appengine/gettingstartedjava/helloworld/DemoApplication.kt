ab01e194c29728a19432b3169437c45b97e9868f
LynAs <szlynas@gmail.com>
2017-08-06 23:55:20 +0600

    init

---
+++ src/main/java/com/example/appengine/gettingstartedjava/helloworld/DemoApplication.kt
@@ -0,0 +1,12 @@
+package com.example.appengine.gettingstartedjava.helloworld
+
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(DemoApplication::class.java, *args)
+}
