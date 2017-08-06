32d1bcbdda3d9ffb58fef80a3611128960807229
LynAs <szlynas@gmail.com>
2017-08-06 23:59:42 +0600

    update

--- src/main/java/com/example/appengine/gettingstartedjava/helloworld/DemoApplication.kt
+++ src/main/java/com/example/appengine/gettingstartedjava/helloworld/DemoApplication.kt
@@ -3,10 +3,23 @@ package com.example.appengine.gettingstartedjava.helloworld
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class DemoApplication
 
 fun main(args: Array<String>) {
     SpringApplication.run(DemoApplication::class.java, *args)
+}
+
+
+@RestController
+class HomeController {
+
+    @GetMapping("/")
+    fun test(): String {
+        return "Ok"
+    }
+
 }
